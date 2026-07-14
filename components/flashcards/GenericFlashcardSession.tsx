1b7ad0e9a75424c5a08f1b37f8e93e7d1ddee26f
jaatdev <kc90040@gmail.com>
2026-07-14 09:22:53 +0530

    fix: prevent flashcard summary from unmounting on completion

--- components/flashcards/GenericFlashcardSession.tsx
+++ components/flashcards/GenericFlashcardSession.tsx
@@ -25,7 +25,6 @@ export default function GenericFlashcardSession({ flashcards, title, onClose }: 
 
   const handleFinish = async () => {
     setIsFinished(true);
-    await exitFullscreen();
   };
 
   const handleClose = async () => {
